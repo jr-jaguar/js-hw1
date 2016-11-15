736dd9a28d030c0e1a6fefe6b3358332c41c8027
jr-jaguar <j.jaguar@rambler.ru>
2016-11-15 20:31:45 +0200

    Conflict2

--- js/hw1.js
+++ js/hw1.js
@@ -1,3 +1,15 @@
-/**
- * Created by jaguar on 15.11.16.
- */
+function Animal(name, type, word) {
+    this.name=name;
+    this.type=type;
+    this.word=word;
+    this.say = function () {
+        var result;
+        result = this.type + "'s " + "name is - " + this.name + " and it say " +this.word;
+        console.log(result);
+
+    }
+}
+
+var dog = new Animal("Bethovin", "Dog", "gau");
+// console.log (typeof cat);
+dog.say();
